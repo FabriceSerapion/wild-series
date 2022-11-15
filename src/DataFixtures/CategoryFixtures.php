9e9e6bf7ac8e774c64b8b53c19bc62d0a7476590
FABRICE SERAPION <serapion.fabrice@hotmail.fr>
2022-11-15 10:23:57 +0100

    generate fixtures + adding foreign key category to program with doctrine

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'action',
+        'aventure',
+        'animation',
+        'fantastique',
+        'horreur',
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        foreach(self::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
